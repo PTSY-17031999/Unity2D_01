01bb3b2d616b0e7f0b01b692c8c72fbd186fbb9e
PTSY-17031999 <132895168+PTSY-17031999@users.noreply.github.com>
2023-07-30 17:20:52 +0700

    sss

--- Dog_climbing_tree/Assets/Textures/Script/TileTower.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class TileTower : MonoBehaviour
-{
-    private bool isAvailable;
-    private Tower TowerCurrent;
-}
